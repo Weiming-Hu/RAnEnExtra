2ce1a19e02d627ab6cbbd9dc949544f0f64ea0e2
Weiming-Hu <weiming@psu.edu>
2020-09-09 10:48:03 -0400

    Fix bugs when all entries are identical.

--- R/boot.fun.ver.R
+++ R/boot.fun.ver.R
@@ -12,24 +12,36 @@
 #
 
 #' RAnEnExtra::boot.fun.ver
-#' 
+#'
 #' @keywords internal
 boot.fun.ver <- function( temp, R, fun = function(d,i){ mean( d[i,1] ) }) {
   temp <- data.frame( val = na.omit( temp ) )
   boot <- rep(NA,3)
 
   check.package('boot')
-  
+
   if( nrow(temp) > 1 ) {
+
     err.boot   <- boot::boot(temp, fun, R=R)
-    bootresult <- boot::boot.ci(err.boot, type="basic")  
-    
-    if ( !is.na(err.boot[[1]])) {
-      boot[1] <- bootresult$t0            
-      boot[2] <- bootresult$basic[4]        
-      boot[3] <- bootresult$basic[5]
+
+    if (length(unique(temp[, 1])) == 1) {
+      # If all values equal
+      boot[1] <- boot[2] <- boot[3] <- err.boot$t0
+
+    } else {
+      # If values do not equal
+      conf <- getOption('RAnEnExtra_boot_conf')
+      if (is.null(conf)) conf <- 0.95
+
+      bootresult <- boot::boot.ci(err.boot, type="basic", conf = conf)
+
+      if ( !is.na(err.boot[[1]])) {
+        boot[1] <- bootresult$t0
+        boot[2] <- bootresult$basic[4]
+        boot[3] <- bootresult$basic[5]
+      }
     }
   }
-  
+
   return(boot)
 }
