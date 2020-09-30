635e94726c7f5bfc60d488e3e0b8351b44d37e26
Weiming <weiming@psu.edu>
2020-09-30 17:47:21 -0400

    Update biasCorrection.R

--- R/biasCorrection.R
+++ R/biasCorrection.R
@@ -98,8 +98,8 @@ biasCorrection <- function(analogs, target.forecasts, historical.forecasts,
   skip.regression <- is.null(regression.forecasts) | is.null(regression.observations)
 
   if (!skip.regression) {
-    stopifnot(is.null(regression.forecasts))
-    stopifnot(is.null(regression.observations))
+    stopifnot(!is.null(regression.forecasts))
+    stopifnot(!is.null(regression.observations))
 
     if (!is.null(dim(regression.forecasts))) {
       regression.forecasts <- as.numeric(regression.forecasts)
